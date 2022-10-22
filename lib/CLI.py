f2e1820e235b447eabe09ad92b723165451ce6a0
Sancho Montana <sancho.montanakbk@gmail.com>
2022-10-22 01:56:43 -0400

    User can now close the map viewer from the spawned window and from the CLI.

--- lib/CLI.py
+++ lib/CLI.py
@@ -1,7 +1,10 @@
-from cmd import Cmd
+import _queue
 import multiprocessing
+from cmd import Cmd
+from multiprocessing import Process
 import GraphicsManager
 import pygame
+
 
 class Console(Cmd):
     prompt = 'dmcli> '
@@ -9,6 +12,7 @@ class Console(Cmd):
 
     def __init__(self):
         super().__init__()
+        self.multiproc_queue = multiprocessing.Queue()
         self.game_window = None
         self.game_thread = None
         self.monsters = []
@@ -27,8 +31,19 @@ class Console(Cmd):
 
     def do_close(self, arg):
         'help'
-        pygame.quite()
-        self.game_window = None
+        try:
+            pygame_state = self.multiproc_queue.get_nowait()
+            if pygame_state == "window_quit":
+                self.game_window = None
+        except _queue.Empty:
+            pass
+
+        if self.game_window:
+            self.multiproc_queue.put("client_quit")
+            pygame.quit()
+            self.game_window = None
+        else:
+            print("Map viewer already closed.")
 
     def do_delete(self, arg):
         'help'
@@ -50,11 +65,23 @@ class Console(Cmd):
 
     def do_quit(self, arg):
         'help'
+        self.do_exit(arg)
 
     def do_start(self, arg):
         'help'
+        try:
+            pygame_state = self.multiproc_queue.get_nowait()
+            if pygame_state == "window_quit":
+                self.game_window = None
+        except _queue.Empty:
+            pass
+
         if self.game_window is None:
-            self.game_window = GraphicsManager.GameWindow()
+            self.game_window = Process(target=GraphicsManager.GameWindow, args=(self.multiproc_queue,))
+            self.game_window.start()
+        else:
+            print("Map viewer already open.")
+
 
     def do_vision(self, arg):
         'help'
@@ -64,6 +91,10 @@ class Console(Cmd):
 
     def do_exit(self, arg):
         print("exiting...")
+        if self.game_window:
+            self.multiproc_queue.put("client_quit")
+            pygame.quit()
+            self.game_window = None
         pygame.quit()
         self.game_window = None
         exit()
--- lib/GraphicsManager.py
+++ lib/GraphicsManager.py
@@ -1,22 +1,36 @@
+import _queue
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from pyautogui import size as getScreenSize
 
-class GameWindow():
-    def __init__(self):
-        self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT = getScreenSize()
+
+class GameWindow:
+    def __init__(self, multiproc_queue):
+        self.multiproc_queue = multiproc_queue
         # Get monitor resolution and set windows dimensions
+        self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT = getScreenSize()
         # Window initialization
         pygame.init()
         pygame.mouse.set_visible(False)
         self.gameDisplay = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.FPS = 1
+        self.FPS = 5
         self.game_exit = False
+        self.run()
 
-    def quit(self):
-        pygame.quit()
+    def run(self):
+        while not self.game_exit:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    self.game_exit = True
+                    self.multiproc_queue.put("window_quit")
+            try:
+                if self.multiproc_queue.get_nowait() == "client_quit":
+                    pygame.quit()
+                    self.game_exit = True
+            except _queue.Empty:
+                pass
+            self.clock.tick(self.FPS)
         return
-
-
