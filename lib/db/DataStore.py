44832004e05419874a757a99d9e45f61c36b5d48
Sancho Montana <sancho.montanakbk@gmail.com>
2022-10-22 00:32:40 -0400

    Database created and schema initialized.

--- lib/db/DataStore.py
+++ lib/db/DataStore.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from lib.db import schema
 
 
 class DataStore:
@@ -7,16 +8,40 @@ class DataStore:
         self.conn = None
         self.working_dir = working_dir
         self.db_filename = "sqlite3.db"
-        self.path_to_db = os.path.join(self.working_dir, "data", self.db_filename)
-        if not os.path.isfile(self.path_to_db):
-            self.create_db()
+        self.db_parent_dir = os.path.join(self.working_dir, "storage")
+        self.path_to_db_file = os.path.join(self.db_parent_dir, self.db_filename)
+        self.sub_folders = ["backgrounds", "characters", "items", "monsters", "players"]
+        self.verify_db()
+        self.verify_folders()
 
-    def create_db(self):
-        print("  - Creating sqlite3 database.")
-        try:
-            self.conn = sqlite3.connect(self.path_to_db)
-            print("  - Formed database:  {}.".format(self.path_to_db))
-        except Exception as e:
-            print("Failed to form database: {}.".format(self.path_to_db))
-            print(e)
+    def verify_db(self):
+        if not os.path.isfile(self.path_to_db_file):
+            print("  - Database not found; initializing.")
+            try:
+                if not os.path.exists(self.db_parent_dir):
+                    os.mkdir(self.db_parent_dir)
+                self.conn = sqlite3.connect(self.path_to_db_file)
+                self.add_schema()
+            except Exception as e:
+                print("  - Failed to form database: {}.".format(self.path_to_db_file))
+                print(e)
+                print("exiting...")
+                exit()
+
+    def verify_folders(self):
+        for sub_folder in self.sub_folders:
+            if not os.path.exists(os.path.join(self.db_parent_dir, sub_folder)):
+                print("  - Creating {} folder.".format(sub_folder))
+                os.mkdir(os.path.join(self.db_parent_dir, sub_folder))
+
+    def add_schema(self):
+        cursor = self.conn.cursor()
+        tables = [schema.create_table_backgrounds,
+                  schema.create_table_characters,
+                  schema.create_table_items,
+                  schema.create_table_monsters,
+                  schema.create_table_players]
+        for table in tables:
+            cursor.execute(table)
+        self.conn.commit()
 
--- lib/db/schema.py
+++ lib/db/schema.py
@@ -6,15 +6,45 @@ CREATE TABLE IF NOT EXISTS backgrounds (
     degree_offset INTEGER DEFAULT 0 NOT NULL,
     flip_horizontal BOOLEAN DEFAULT FALSE,
     flip_vertical BOOLEAN DEFAULT FALSE,
-    mode TEXT DEFAULT fit
+    mode TEXT DEFAULT "fit"
 );"""
-create_table_pcs = """
-CREATE TABLE IF NOT EXISTS pcs (
+create_table_characters = """
+CREATE TABLE IF NOT EXISTS characters (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     filename TEXT NOT NULL,
     center_x INTEGER NOT NULL,
-    center_y INTEGER NOT NULL
+    center_y INTEGER NOT NULL,
     radius INTEGER NOT NULL
 );"""
-
+create_table_items = """
+CREATE TABLE IF NOT EXISTS items (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    filename TEXT NOT NULL,
+    token BOOLEAN NOT NULL,
+    center_x INTEGER DEFAULT 0 NOT NULL,
+    center_y INTEGER DEFAULT 0 NOT NULL,
+    radius INTEGER DEFAULT 0 NOT NULL,
+    grid_width INTEGER DEFAULT 0 NOT NULL,
+    grid_height INTEGER DEFAULT 0 NOT NULL
+);"""
+create_table_monsters = """
+CREATE TABLE IF NOT EXISTS monsters (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    filename TEXT NOT NULL,
+    center_x INTEGER DEFAULT 0 NOT NULL,
+    center_y INTEGER DEFAULT 0 NOT NULL,
+    radius INTEGER DEFAULT 0 NOT NULL,
+    grid_diameter INTEGER DEFAULT 1 NOT NULL
+);"""
+create_table_players = """
+CREATE TABLE IF NOT EXISTS players (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    filename TEXT NOT NULL,
+    center_x INTEGER NOT NULL,
+    center_y INTEGER NOT NULL,
+    radius INTEGER NOT NULL
+);"""
