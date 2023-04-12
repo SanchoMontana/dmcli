78f0c8be9bfaceb805c6b388b791bba217606d26
Sancho Montana <sancho.montanakbk@gmail.com>
2023-04-11 21:32:27 -0400

    Each commands can now complete the first argument afterward... I do not quite know how i will be able to extend this to being able to autocomplete the next layer of arguments (e.g. "create monster <TAB>" ideally should display all options for creating a monster). This will be interesting.

--- lib/CLI.py
+++ lib/CLI.py
@@ -6,6 +6,7 @@ import pygame
 import sys
 import inspect
 from actions import commands
+import readline
 
 
 
@@ -30,6 +31,43 @@ class Console(Cmd):
             setattr(self, 'do_' + command.title, command.do)
 
     def completenames(self, text, *ignored):
+        # This completion logic is just for the first word in each command.
         # This is so flippin pythonic...
         all_commands = [n.split("_", 1)[0].lower() for n, _ in inspect.getmembers(commands, inspect.isclass) if n.endswith("_Command")]
-        return [a for a in all_commands if a.startswith(text)]
+        options = [a for a in all_commands if a.startswith(text)]
+        if len(options) == 1:
+            if text == options[0]:
+                readline.insert_text(" ")
+                return []
+            else:
+                options[0] += " "
+        return options
+    
+    def completedefault(self, text, line, begidx, endidx):
+        """Method called to complete an input line when no command-specific
+        complete_*() method is available.
+        """
+        cmd_class = None
+        all_options = []
+        smart_options = []
+        if len(line.split(" ")) == 2:
+            base_cmd = line.split(" ")[0]
+            for n, _ in inspect.getmembers(commands, inspect.isclass):
+                cmd, ending = n.split('_', 1) # TODO: This may throw an error if there is a class in commands.py that doens't have an underscore in the name.
+                if cmd.lower().startswith(base_cmd) and ending == "Command":
+                    cmd_class = getattr(commands, n)
+            if cmd_class:
+                if hasattr(cmd_class, "options"):
+                    all_options = cmd_class.options
+            if all_options:
+                for i in all_options:
+                    if i.startswith(text):
+                        smart_options.append(i)
+                if len(smart_options) == 1:
+                    if text.split(" ", 1)[0] == smart_options[0]:
+                        # if tab completed when the arg is already complete.
+                        readline.insert_text(" ")
+                        return []
+                    else:
+                        smart_options[0] += " "
+        return smart_options
